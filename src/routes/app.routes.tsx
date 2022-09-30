8c20fe782766d84fbe0c88a39b8f1f68b70ad7e1
Gui-dev <adrianon10@gmail.com>
2022-09-30 03:29:35 -0300

    feat: add routes component

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { Home } from '../screens/Home'
+import { Game } from '../screens/Game'
+
+export const AppRoutes = () => {
+  const { Navigator, Screen } = createNativeStackNavigator()
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="Game" component={Game} />
+    </Navigator>
+  )
+}
